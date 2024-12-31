2d88818510ca10b3a4f78fd4c7d319d9152b0738
battanair <battanedd@gmail.com>
2024-12-31 13:27:24 +0000

    yeah

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { NavLink } from 'react-router';
 
 export default function Navbar() {
   return (
@@ -17,7 +18,7 @@ export default function Navbar() {
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 , color: 'black'}}>
             <MenuIcon />
           </IconButton>
-          <img src="./logoziater.png" alt="Logo" style={{ height: "60px", marginLeft: "10px" }} />
+          <NavLink to="/"><img src="./logoziater.png" alt="Logo" style={{ height: "60px", marginLeft: "10px" }} /></NavLink>
         </Toolbar>
       </AppBar>
     </Box>
--- src/routing/routerConfig.js
+++ src/routing/routerConfig.js
@@ -1,4 +1,5 @@
 import Obra from "../pages/obra";
+import Home from "../pages/home";
 import Persona from "../pages/persona"
 
 
@@ -6,10 +7,14 @@ export const routeConfig = [
   {
     name: "Root",
     path: "/",
+    page: <Home />,
+  },{
+    name: "Obra",
+    path: "/obra/",
     page: <Obra />,
   },{
     name: "Persona",
-    path: "/persona",
+    path: "/persona/",
     page: <Persona />,
   },
  
